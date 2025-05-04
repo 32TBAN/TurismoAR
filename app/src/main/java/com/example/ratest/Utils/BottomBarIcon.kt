40bca07bcc80b9ff704c3d60b88aa6c51c258e3d
esteban <estebanguanoluisa55@gmail.com>
2025-05-03 22:14:49 -0500

    se soluciono conflictos con la routes

--- app/src/main/java/com/example/ratest/Utils/BottomBarIcon.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ratest.Utils
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-sealed class BottomBarIcon {
-    data class Vector(val icon: ImageVector) : BottomBarIcon()
-    data class Drawable(val resId: Int) : BottomBarIcon()
-}
